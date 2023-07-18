e8e655ce5bc16b03957939bfbc7a725f1d6b681d
PheeHandy <PheeHandy@gmail.com>
2023-07-19 01:25:31 +0700

    change Cookie function

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -1,15 +1,11 @@
-window.addEventListener("message", receiveMessage, false);
-
-function receiveMessage(event)
-{
-  if (event.origin !== "https://script.google.com/macros/s/AKfycbw9tlrF7-BbRLq84vj1CsC_kVa6to9kaHB5Wu9nk5sdnPEdBqiSEoYacN4mail7_HSP8Q/exec") // replace with the origin of your Google Apps Script
-    return;
-
-  // assuming you sent the data as a JSON object
+window.addEventListener("message", function(event) {
+  // Parse the received data
   var data = JSON.parse(event.data);
 
-  // do whatever you need to with the data
-  // for example, set it as a cookie
-  document.cookie = "name=" + data.name;
-  console.log("cookie = " + cookie);
-}
+  // Save the data to localStorage
+  localStorage.setItem('name', data.name);
+  var name = localStorage.getItem('name');
+  console.log("name =" + data.name);
+  console.log(name);
+  
+});
