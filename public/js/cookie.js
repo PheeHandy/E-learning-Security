0f73930d869defe36c3daac037462471773c2434
PheeHandy <PheeHandy@gmail.com>
2023-07-19 01:09:59 +0700

    test Cookie function

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -2,7 +2,7 @@ window.addEventListener("message", receiveMessage, false);
 
 function receiveMessage(event)
 {
-  if (event.origin !== "https://script.google.com/macros/s/AKfycbxjFROVgYidAFtfb7o0zjWznNnZ_Ehok5mo3l3Pzczpabl-oGHvgQ-kyDgNg2gQcPLe_A/exec") // replace with the origin of your Google Apps Script
+  if (event.origin !== "https://script.google.com/macros/s/AKfycbw9tlrF7-BbRLq84vj1CsC_kVa6to9kaHB5Wu9nk5sdnPEdBqiSEoYacN4mail7_HSP8Q/exec") // replace with the origin of your Google Apps Script
     return;
 
   // assuming you sent the data as a JSON object
