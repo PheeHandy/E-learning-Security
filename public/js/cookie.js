a4178a97ffa502b4d44ba7596671c258d40eb572
PheeHandy <PheeHandy@gmail.com>
2023-07-19 01:36:58 +0700

    Debug Cookie.js

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -1,7 +1,8 @@
 window.addEventListener("message", function(event) {
   // Parse the received data
+  console.log("Received message from: " + event.origin);  // Debug line
+  console.log("Received message: " + event.data);  // Debug line
   var data = JSON.parse(event.data);
-
   // Save the data to localStorage
   localStorage.setItem('name', data.name);
   var name = localStorage.getItem('name');
