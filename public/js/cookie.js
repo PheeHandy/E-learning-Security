0c210e18c80182082b81d59909730814dd4abc28
PheeHandy <PheeHandy@gmail.com>
2023-07-19 00:47:18 +0700

    Update cookie.js

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -2,7 +2,7 @@ window.addEventListener("message", receiveMessage, false);
 
 function receiveMessage(event)
 {
-  if (event.origin !== "https://script.google.com") // replace with the origin of your Google Apps Script
+  if (event.origin !== "https://script.google.com/macros/s/AKfycbyczVFZZ2ijHFYB1x9NjkTcILGNhppblJdxGB3SFfbJKPEgfsU3ILCGd5_IQz9Pf__Kaw/exec") // replace with the origin of your Google Apps Script
     return;
 
   // assuming you sent the data as a JSON object
@@ -11,4 +11,5 @@ function receiveMessage(event)
   // do whatever you need to with the data
   // for example, set it as a cookie
   document.cookie = "name=" + data.name;
+  console.log("cookie = " + cookie);
 }
